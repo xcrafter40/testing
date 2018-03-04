9366804d1cbec32a836405e1fd4aa67f49dfe1f1
xcrafter40 <xcrafter.40@gmail.com>
2018-03-04 09:57:22 -0800

    Playing with the code

--- helloworld/application.py
+++ helloworld/application.py
@@ -7,11 +7,13 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return "You used get!"
+    #return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    return "You used post!"
+    #return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
