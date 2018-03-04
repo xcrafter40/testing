a7cbc643fdedb738c6526c25055b5b084e69e7ba
xcrafter40 <xcrafter.40@gmail.com>
2018-03-04 10:37:34 -0800

    fixes

--- helloworld/application.py
+++ helloworld/application.py
@@ -7,13 +7,13 @@ application = Flask(__name__)
 
 @application.route('/', methods=['GET'])
 def get():
-    return "You used get!"
-    #return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    #return "You used get!"
+    return Response(json.dumps({'Output': 'FOO BAR!!!'}), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
-    return "You used post!"
-    #return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    #return "You used post!"
+    return Response(json.dumps({'Output': 'ABC'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
     flaskrun(application)
